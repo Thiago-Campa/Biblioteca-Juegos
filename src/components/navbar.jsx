8ef3d0b595d9debc1f36dbba37a3e1ef495699dd
Thiago-Campa <thiagocampagnaro9@gmail.com>
2026-05-12 20:05:55 -0300

    Actualizacion de paginas y navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,11 +10,11 @@ function Navbar() {
       color: "#fff"
     }}>
       <Link to="/" style={{ color: "#fff" }}>Inicio</Link>
-      <Link to="/juegos" style={{ color: "#fff" }}>Juegos</Link>
+      <Link to="/games" style={{ color: "#fff" }}>Juegos</Link>
       <Link to="/login" style={{ color: "#fff" }}>Login</Link>
       <Link to="/register" style={{ color: "#fff" }}>Registro</Link>
     </nav>
-  );
+  ); 
 }
 
 export default Navbar;
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,16 +1,133 @@
 function Register() {
   return (
-    <div>
-      <h2>Registrarse</h2>
-      <form>
-        <input type="text" placeholder="Nombre" />
-        <br />
-        <input type="email" placeholder="Email" />
-        <br />
-        <input type="password" placeholder="Contraseña" />
-        <br />
-        <button>Crear cuenta</button>
-      </form>
+    <div
+      style={{
+        backgroundColor: "#1f1f1f",
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "30px",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#173b9c",
+          width: "420px",
+          padding: "40px",
+          borderRadius: "10px",
+          color: "white",
+        }}
+      >
+        <h1
+          style={{
+            textAlign: "center",
+            marginBottom: "30px",
+          }}
+        >
+          Crear Cuenta
+        </h1>
+
+        {/* NOMBRE */}
+        <div style={{ marginBottom: "20px" }}>
+          <label>Nombre</label>
+
+          <input
+            type="text"
+            placeholder="Ingrese su nombre"
+            style={{
+              width: "100%",
+              padding: "12px",
+              marginTop: "8px",
+              borderRadius: "5px",
+              border: "none",
+              outline: "none",
+            }}
+          />
+        </div>
+
+        {/* EMAIL */}
+        <div style={{ marginBottom: "20px" }}>
+          <label>Email</label>
+
+          <input
+            type="email"
+            placeholder="Ingrese su email"
+            style={{
+              width: "100%",
+              padding: "12px",
+              marginTop: "8px",
+              borderRadius: "5px",
+              border: "none",
+              outline: "none",
+            }}
+          />
+        </div>
+
+        {/* PASSWORD */}
+        <div style={{ marginBottom: "20px" }}>
+          <label>Contraseña</label>
+
+          <input
+            type="password"
+            placeholder="Ingrese su contraseña"
+            style={{
+              width: "100%",
+              padding: "12px",
+              marginTop: "8px",
+              borderRadius: "5px",
+              border: "none",
+              outline: "none",
+            }}
+          />
+        </div>
+
+        {/* CONFIRM PASSWORD */}
+        <div style={{ marginBottom: "25px" }}>
+          <label>Confirmar Contraseña</label>
+
+          <input
+            type="password"
+            placeholder="Repita su contraseña"
+            style={{
+              width: "100%",
+              padding: "12px",
+              marginTop: "8px",
+              borderRadius: "5px",
+              border: "none",
+              outline: "none",
+            }}
+          />
+        </div>
+
+        {/* BUTTON */}
+        <button
+          style={{
+            width: "100%",
+            padding: "12px",
+            backgroundColor: "#1f1f1f",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+            fontSize: "16px",
+            fontWeight: "bold",
+          }}
+        >
+          Registrarse
+        </button>
+
+        {/* TEXTO */}
+        <p
+          style={{
+            textAlign: "center",
+            marginTop: "20px",
+            fontSize: "14px",
+          }}
+        >
+          ¿Ya tenés cuenta? Iniciá sesión
+        </p>
+      </div>
     </div>
   );
 }
